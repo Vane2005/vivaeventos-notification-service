f35ceb805e4806e119ed1e830c0f8305ef1e8ba3
001Miguel15cn <miguel.arboleda@correounivalle.edu.co>
2026-05-29 18:01:41 -0500

    intento de solucionde package -2

--- src/main/java/com/edu/univalle/vivaeventosnotificationservice/VivaeventosNotificationServiceApplication.java
+++ src/main/java/com/edu/univalle/vivaeventosnotificationservice/VivaeventosNotificationServiceApplication.java
@@ -1,4 +1,4 @@
-package co.edu.univalle.vivaeventosnotificationservice;
+package com.edu.univalle.vivaeventosnotificationservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
